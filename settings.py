2a4b4b88b085f290b8016a82b4b49cbf93d39125
Luciano Silvi <lucianosilvi@gmail.com>
2018-10-16 22:05:49 +0200

    merge settings

--- settings.py
+++ settings.py
@@ -5,8 +5,7 @@ MAINNET_SOCKET_FILE_PATH = ''
 NETWORK_TO_USE = 'local'  # choose between 'local' --> ganache (use this if you are developing with truffle and start
 # the local blockchain with truffle develop) 'in_memory_test_rpc' --> simple in memory blockchain (use this if you are
 # not using truffle to start the development blockchain)  or 'rinkeby' --> rinkeby testnet
-CONTRACTS_FOLDER = contracts_folder = os.path.join(os.path.join(project_directory, 'truffle_project'), 'contracts')
+CONTRACTS_FOLDER = contracts_folder = os.path.join(os.path.join(project_directory, 'upgradeable_voting_smart_contracts'), 'contracts')
 ETHER_WALLET_ID_TO_USE = 0
 PROVIDER_RATING_CONTRACT_EXISTING_ON_BLOCKCHAIN = False 
 PROVIDER_RATING_CONTRACT_ADDRESS = '0x8DA7eB4Ec3A4c1291797e13DB723f9046afF4a1C'
-
