f3ec197a0d07f1954a04529a0067e8812f7cc8d1
Matyas <matthias.karacsonyi@gmail.com>
2018-10-25 11:57:48 +0200

    added loom dappchain to truffle project

---
+++ public_voting_dapp/contracts/Provider.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+contract Provider {
+    bytes8 public geohash;
+    bytes12 public csc;
+    bytes32 providerName;
+    string providerPostalAddress;
+    bytes32 providerIdentificationNumber;
+
+    // geohash of up to 10 characters
+    function Provider(bytes8 _geohash, bytes32 _providerName, string _providerPostalAddress,
+        bytes32 _providerIdentificationNumber){
+
+      geohash = _geohash;
+      csc = computeCSC(geohash, address(this));
+      providerName = _providerName;
+      providerPostalAddress = _providerPostalAddress;
+      providerIdentificationNumber = _providerIdentificationNumber;
+    }
+
+    function computeCSC(bytes8 geohash_arg, address addr) internal returns(bytes12) {
+      return bytes12(sha3(geohash_arg, addr));
+    }
+}
