706137dd967127ad89b7acc0e364eec94c8a92b5
Matyas <matthias.karacsonyi@gmail.com>
2018-11-27 12:09:00 +0100

    cleaned up project structure

--- public_voting_backend/public_voting/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PublicVotingConfig(AppConfig):
-    name = 'public_voting'
--- settings.py
+++ settings.py
@@ -2,14 +2,14 @@ import os
 project_directory =os.path.dirname(os.path.abspath(__file__))
 RINKEBY_SOCKET_FILE_PATH = '/tmp/rinkeby/geth.ipc'
 MAINNET_SOCKET_FILE_PATH = ''
-NETWORK_TO_USE = 'local'  # choose between 'local' --> ganache (use this if you are developing with truffle and start
+NETWORK_TO_USE = 'rinkeby'  # choose between 'local' --> ganache (use this if you are developing with truffle and start
 # the local blockchain with truffle develop) 'in_memory_test_rpc' --> simple in memory blockchain (use this if you are
 # not using truffle to start the development blockchain)  or 'rinkeby' --> rinkeby testnet
 CONTRACTS_FOLDER = contracts_folder = os.path.join(os.path.join(project_directory, 'truffle_project'), 'contracts')
 CONTRACTS_ABI_FOLDER = os.path.join(
     os.path.join(os.path.join(project_directory, 'truffle_project'), 'build'), 'contracts')
 
-ETHER_WALLET_ID_TO_USE = 9
+ETHER_WALLET_ID_TO_USE = 7
 
 UPGRADABLE_VOTING_PROXY_SMART_CONTRACT_ADDRESS = '0xbb60d495d500100c0be856d6d5cbea3b00fa07ac'
 PROVIDER_RATING_CONTRACT_EXISTING_ON_BLOCKCHAIN = False
